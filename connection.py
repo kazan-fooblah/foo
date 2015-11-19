3704e1d2759a2f4fb5231b618374df1b36e2ffe0
Konstantin Makarychev <mrsimpson@inbox.ru>
2015-11-19 05:45:57 +0300

    Debug

--- connection.py
+++ connection.py
@@ -39,7 +39,7 @@ class Connection:
         # }
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-        sock.sendto(m, (MCAST_GRP, MCAST_PORT))
+        sock.sendto(message, (MCAST_GRP, MCAST_PORT))
         sock.close()
 
     @mainthread
