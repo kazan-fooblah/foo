d8fc73a08376ec0f74259b6d186cb8b7288e3b5f
Konstantin Makarychev <mrsimpson@inbox.ru>
2015-11-19 06:01:42 +0300

    Debug

--- accelerometer.py
+++ accelerometer.py
@@ -46,4 +46,4 @@ class Accelerometer:
 
     @staticmethod
     def accelerometer_representation():
-        return "X = %.2f Y = %.2f Z = %2.f" % (accelerometer.acceleration[0], accelerometer.acceleration[1], accelerometer.acceleration[2])
+        return "X = %.2f Y = %.2f Z = %2.f" % (float(accelerometer.acceleration[0]), float(accelerometer.acceleration[1]), float(accelerometer.acceleration[2]))
